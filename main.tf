5be8bd22779a59c3da3728b77402b197f9ff39d4
Shakeeb Ahmed <m.shakeebahmed@gmail.com>
2023-01-04 18:18:15 +0000

    c

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ terraform {
 
 provider "aws" {
   region = "us-west-2"
-  access_key = "$(var.AWS_ACCESS_KEY}"
-  secret_key = "$(var.AWS_SECRET_KEY}"
+  # access_key = "$(var.AWS_ACCESS_KEY}"
+  # secret_key = "$(var.AWS_SECRET_KEY}"
 }
 
 resource "aws_cloudwatch_dashboard" "dsahboard1" {
